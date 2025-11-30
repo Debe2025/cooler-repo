86de1a6e9c9ac5e89912ecaf37309357d9f9c00b
Debe2025 <Debe2025@users.noreply.github.com>
2025-11-29 17:33:10 -0700

    Add script.module.addonsignals stub for Netflix

---
+++ script.module.addonsignals/default.py
@@ -0,0 +1,12 @@
+
+# placeholder AddonSignals-compatible stub
+# real add-on would provide full API; here we only satisfy imports for Netflix if it doesn't use it deeply
+
+def sendSignal(*args, **kwargs):
+    pass
+
+def registerSlot(*args, **kwargs):
+    pass
+
+def unregisterSlot(*args, **kwargs):
+    pass
